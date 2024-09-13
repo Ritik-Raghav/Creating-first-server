4a88a5629faf8d1a310723bb04869dfe20faeb95
Ritik Raghav <ritikrghv313@gmail.com>
2024-09-13 20:43:57 +0530

    using express

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const http = require('http');
-const routes = require('./routes');
 
+const express = require('express');
 
-const server = http.createServer(routes);
+const app = express();
 
-server.listen(3000);
+app.use((req, res, next) => {
+    console.log('in the middleware');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('in the another middleware');
+    res.send('<h1>Hello from express!</h1>');
+
+});
+
+app.listen(3000);
